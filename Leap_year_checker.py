92bfb71966e26cabfa5e955edf27f015bb82bd42
Chatak1 <72140766+Chatak1@users.noreply.github.com>
2020-10-20 06:49:05 +0530

    Update Leap_year_checker.py
    Your Logic was quite incorrect. I tried my best to correct it. Hope you merge it. Always try to take inputs from users (like I have made changes in line no. 3 )

--- Leap_year_checker.py
+++ Leap_year_checker.py
@@ -1,12 +1,12 @@
 # Python program to check if year is a leap year or not
 
-year = 2000
+year = int(input("Enter Year to check")) # this will take input from user
 
 
 
-if (year / 4) == 0:
-   if (year / 10) == 0:
-       if (year % 4) == 0:
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
            print("{0} is a leap year".format(year))
        else:
            print("{0} is not a leap year".format(year))
