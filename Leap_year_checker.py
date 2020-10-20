7598329bf37b19890577af53a1f40bcdd2d37d04
Chatak1 <72140766+Chatak1@users.noreply.github.com>
2020-10-20 06:58:31 +0530

    Update Leap_year_checker.py
    I  have commented on your quite incorrect logic (for your reference). Hope you like it.

--- Leap_year_checker.py
+++ Leap_year_checker.py
@@ -4,8 +4,8 @@ year = int(input("Enter Year to check")) # this will take input from user
 
 
 
-if (year % 4) == 0:
-   if (year % 100) == 0:
+if (year % 4) == 0:        #for correct output here instead of division modulus is axpected for correct output
+   if (year % 100) == 0:   #instead of 10 use 100   
        if (year % 400) == 0:
            print("{0} is a leap year".format(year))
        else:
